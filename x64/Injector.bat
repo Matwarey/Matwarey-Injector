fb2a7ce96ed4c22adba44589c4fde10e2ba2151a
Matwarey <matthewbr106@gmail.com>
2022-07-03 16:18:10 -0400

    Add More Features
    Features Added:
    File Picker (Beta, not recommand when running a game on low end hardware)
    
    Confirm selected file/path
    Changes to improve Matwarey Injector.

--- x64/Injector.bat
+++ x64/Injector.bat
@@ -1,15 +1,15 @@
 :: Startup Commands
 @echo off
-@Mode 47,13
+@Mode 77,13
 title Matwarey Injector Stable
 
 :: Load Main Menu
 Batbox /h 0
 
 :Loop
-cd C:\Users\Matth\Downloads\Matwarey Injector\x64\
+cd C:\Users\%username%\Downloads\Matwarey Injector\x64\
 cls
-Call Button  6 4 "Inject" 29 4 "Eject" 18 8 "Exit" # Press
+Call Button  2 4 "Inject" 65 4 "Eject" 33 4 "Exit" # Press
 Getinput /m %Press% /h 70
 
 :: Check for the pressed buttons below 
@@ -37,25 +37,109 @@ cls
 echo What's the name of the process?
 set /p ProcessName=Name of Process: 
 cls
-echo What's the Path of the DLL?
-set /p DLLName=Path: 
+:ProcessNamefileQNA
+echo Do you want to copy file path or open file selecter?
+echo.
+echo 1) Copy File Path (Great for Low End Computers)
+echo 2) Open File Selecter (Faster way to search for DLL)
+echo.
+set /p QNAA=#:
+if %QNAA%==1 (goto PNFileCopyPath)
+if %QNAA%==2 (goto FileSelecterPN)
+goto ProcessNamefileQNA
+
+:PNFileCopyPath
 cls
-goto inject
+echo.
+echo Example: C:/Users/%username%/Downloads/ModMenu.dll
+echo Copy File Directory Below:
+echo.
+set /p FileName=#:
+cls
+:ConfirmPNFile
+echo %FileName% has been wrote here, is this correct? 
+echo.
+echo 1) Yes
+echo 2) No
+echo.
+set /p QNAXB=#: 
+if %QNAXB%==1 (goto inject)
+if %QNAXB%==2 (goto PNFileCopyPath)
+goto ConfirmPNFile
+
+:FileSelecterPN
+cls
+echo Select a DLL File.
+set pwshcmd=powershell -noprofile -command "&{[System.Reflection.Assembly]::LoadWithPartialName('System.windows.forms') | Out-Null;$OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog; $OpenFileDialog.ShowDialog()|out-null; $OpenFileDialog.FileName}"
+for /f "delims=" %%I in ('%pwshcmd%') do set "FileName=%%I"
+:ConfirmPNFileS
+echo %FileName% has been selected, is this correct?
+echo.
+echo 1) Yes
+echo 2) No
+echo.
+set /p QNA=#: 
+if %QNA%==1 (goto inject)
+if %QNA%==2 (goto FileSelecterPN)
+cls
+goto ConfirmPNFileS
 
 :ProcessIDInjectQNA
 cls
 echo What's the ID of the process?
 set /p ProcessID=ID of Process: 
 cls
-echo What's the Path of the DLL?
-set /p DLLName=Path: 
+:ProcessIDQNA
+echo Do you want to copy file path or open file selecter?
+echo.
+echo 1) Copy File Path (Great for Low End Computers)
+echo 2) Open File Selecter (Faster way to search for DLL)
+echo.
+set /p QNAX=#:
+if %QNAX%==1 (goto IDFileCopyPath)
+if %QNAX%==2 (goto FileSelecterID)
+goto ProcessIDQNA
+
+:IDFileCopyPath
 cls
-goto injectID
+echo.
+echo Example: C:/Users/%username%/Downloads/ModMenu.dll
+echo Copy File Directory Below:
+echo.
+set /p FileName=#: 
+cls
+:ConfirmIDFile
+echo %FileName% has been wrote here, is this correct?
+echo.
+echo 1) Yes
+echo 2) No
+echo.
+set /p QNAXX=#: 
+if %QNAXX%==1 (goto injectID)
+if %QNAXX%==2 (goto IDFileCopyPath)
+goto ConfirmIDFile
+
+:FileSelecterID
+cls
+echo Select a DLL File.
+set pwshcmd=powershell -noprofile -command "&{[System.Reflection.Assembly]::LoadWithPartialName('System.windows.forms') | Out-Null;$OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog; $OpenFileDialog.ShowDialog()|out-null; $OpenFileDialog.FileName}"
+for /f "delims=" %%I in ('%pwshcmd%') do set "FileName=%%I"
+:ConfirmIDFileS
+echo %FileName% has been selected, is this correct?
+echo.
+echo 1) Yes
+echo 2) No
+echo.
+set /p QNAT=#: 
+if %QNAT%==1 (goto injectID)
+if %QNAT%==2 (goto FileSelecterID)
+cls
+goto ConfirmIDFileS
 
 :inject
 cls
 :: Inject into Process Name
-injector.exe -n %ProcessName% -i %DLLName%
+injector.exe -n %ProcessName% -i %FileName%
 :: Notify Person
 echo Injector.exe Task Completed!
 echo.
@@ -65,7 +149,7 @@ goto Loop
 :injectID
 cls
 :: Injects into Process PID
-injector.exe -p %ProcessID% -i %DLLName%
+injector.exe -p %ProcessID% -i %FileName%
 :: Notify Person
 echo Injector.exe Task Completed!
 echo.
@@ -74,8 +158,8 @@ goto Loop
 
 :Eject
 cls
-injector.exe -n %ProcessName% -e %DLLName% >nul
-injector.exe -p %ProcessID% -e %DLLName% >nul
+injector.exe -n %ProcessName% -e %FileName% >nul
+injector.exe -p %ProcessID% -e %FileName% >nul
 echo DLL Ejected.
 pause
 goto Loop
