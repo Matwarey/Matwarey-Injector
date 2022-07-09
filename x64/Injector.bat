7158f460287370f938351261b9813c0702442129
Matwarey <matthewbr106@gmail.com>
2022-07-09 15:55:46 -0400

    Dev Mode + Unsupported Editions
    Added Features:
    
    UnsupportedWindowsCheck is basically a check that checks what windows edition you are on, like if your on Windows Vista or Windows 2000, You can't use Matwarey Injector
    
    Dev Menu is a Dev Menu full for things for Developers and Features for Developers that can be coded for Matwarey Injector, Plugins might be added soon to the Dev Menu/Mode.

--- x64/Injector.bat
+++ x64/Injector.bat
@@ -7,7 +7,7 @@ title Matwarey Injector x64 Stable
 cd /
 IF EXIST "%PROGRAMFILES(X86)%" (set OS=x64) else (set OS=x86)
 if %username%==Matth if %OS%==x64 goto Dev
-if %OS%==x64 goto Loop
+if %OS%==x64 goto BeforeLoop
 
 :: Setup Alert
 if %OS%==x86 set Unsupported=1
@@ -25,25 +25,83 @@ if %Unsupported%==1 if %OS%==x86 cd C:\Users\%username%\Downloads\Matwarey-Injec
 if %Unsupported%==1 if %OS%==x86 start injector.bat
 if %Unsupported%==1 if %OS%==x86 exit
 
+:UnsupportedWindowsCheck
+ver | findstr /i "6\.0\." > nul && (echo Windows Vista isn't Supported! & goto UnsupportedWindowsEdition)
+ver | findstr /i "5\.2\." > nul && (echo Windows XP (x64) isn't Supported! & goto UnsupportedWindowsEdition)
+ver | findstr /i "5\.1\." > nul && (echo Windows XP (x86) isn't Supported! & goto UnsupportedWindowsEdition)
+ver | findstr /i "5\.0\." > nul && (echo Windows 2000 isn't Supported! & goto UnsupportedWindowsEdition)
+
+:UnsupportedWindowsEdition
+echo You can't use Matwarey Injector.
+pause
+exit
+
 :Dev
 :: Check if in developer mode folder (no changes special happens really..)
 if %username%==Matth cd C:\Users\Matth\Documents\GitHub\Matwarey-Injector\x64
 if %username%==Matth title Matwarey Injector x64 Dev
 if %username%==Matth goto Loop
 
+:BeforeLoop
+cd C:/Users/%username%/Downloads/Matwarey-Injector/x64/
+goto Loop
+
 :: Load Main Menu
 :Loop
-cd C:/Users/Matth/Downloads/Matwarey-Injector/x64/
+title Matwarey Injector x64 Stable
+if %username%==Matth title Matwarey Injector x64 Dev
 Batbox /h 0
 
 cls
-Call Button  2 4 "Inject" 65 4 "Eject" 33 4 "Exit" # Press
+Call Button  2 4 "Inject" 65 4 "Eject" 32 4 "Dev Mode" # Press
 Getinput /m %Press% /h 70
 
 :: Check for the pressed buttons below
 if %errorlevel%==1 (goto InjectorQNA)
 if %errorlevel%==2 (goto Eject)
-if %errorlevel%==3 (exit)
+if %errorlevel%==3 (goto DevMode)
+goto Loop
+
+:DevMode
+cls
+title Matwarey Injector Developer Menu
+echo Note: Please know what you are doing before messing around in developer menu!
+echo.
+echo.
+echo 1) Dev Mode Command Prompt
+echo.
+echo 2) Custom Injection (WIP)
+echo.
+echo 3) Version Info
+echo.
+echo 4) Back to Main Menu
+echo.
+set /p nsw=#: 
+if %nsw%==1 (goto DMCP)
+if %nsw%==2 (goto CDLLI)
+if %nsw%==3 (goto VI)
+if %nsw%==4 (goto Loop)
+goto DevMode
+
+:VI
+cls
+echo -=System Infomation-=
+echo.
+echo Processer: %Processer_Architecture%
+ver | findstr /i "6\.1\." > nul && (echo OS: Windows 7)
+ver | findstr /i "6\.2\." > nul && (echo OS: Windows 8)
+ver | findstr /i "6\.3\." > nul && (echo OS: Windows 8.1)
+ver | findstr /i "10\.0\." > nul && (echo OS: Windows 10)
+echo Batch Version: 3.0.0
+pause
+goto DevMode
+
+:DMCP
+cls
+echo Starting Dev Mode Command Prompt
+echo Matwarey Injector Dev Command Line v1.0.0
+cmd.exe /k "title Matwarey Command Prompt && prompt #-: "
+echo Dev Mode Command Prompt ended!
 goto Loop
 
 :InjectorQNA
